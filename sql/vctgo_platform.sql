ba02c8cd7070cabdc898d121d21e698cac679578
xiaowu <weicj0426@163.com>
2023-04-11 09:25:45 +0800

    [FEAT]增加job初始化脚本

--- sql/vctgo_platform.sql
+++ sql/vctgo_platform.sql
@@ -959,6 +959,250 @@ CREATE TABLE `sys_user_role` (
 -- ----------------------------
 -- Records of sys_user_role
 -- ----------------------------
+-- ----------------------------
+-- Table structure for xxl_job_group
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_group`;
+CREATE TABLE `xxl_job_group`  (
+                                  `id` int NOT NULL AUTO_INCREMENT,
+                                  `app_name` varchar(64)  NOT NULL COMMENT '执行器AppName',
+                                  `title` varchar(12)  NOT NULL COMMENT '执行器名称',
+                                  `address_type` tinyint NOT NULL DEFAULT 0 COMMENT '执行器地址类型：0=自动注册、1=手动录入',
+                                  `address_list` text  NULL COMMENT '执行器地址列表，多地址逗号分隔',
+                                  `update_time` datetime NULL DEFAULT NULL,
+                                  `tenant_id` bigint NULL DEFAULT NULL,
+                                  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_group
+-- ----------------------------
+INSERT INTO `xxl_job_group` VALUES (1, 'xxl-job-executor-sample', '示例执行器', 0, 'http://192.168.2.12:9999/', '2023-03-18 20:21:24', 9999);
+
+-- ----------------------------
+-- Table structure for xxl_job_info
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_info`;
+CREATE TABLE `xxl_job_info`  (
+                                 `id` int NOT NULL AUTO_INCREMENT,
+                                 `job_group` int NOT NULL COMMENT '执行器主键ID',
+                                 `job_desc` varchar(255)  NOT NULL,
+                                 `add_time` datetime NULL DEFAULT NULL,
+                                 `update_time` datetime NULL DEFAULT NULL,
+                                 `author` varchar(64)  NULL DEFAULT NULL COMMENT '作者',
+                                 `alarm_email` varchar(255)  NULL DEFAULT NULL COMMENT '报警邮件',
+                                 `schedule_type` varchar(50)  NOT NULL DEFAULT 'NONE' COMMENT '调度类型',
+                                 `schedule_conf` varchar(128)  NULL DEFAULT NULL COMMENT '调度配置，值含义取决于调度类型',
+                                 `misfire_strategy` varchar(50)  NOT NULL DEFAULT 'DO_NOTHING' COMMENT '调度过期策略',
+                                 `executor_route_strategy` varchar(50)  NULL DEFAULT NULL COMMENT '执行器路由策略',
+                                 `executor_handler` varchar(255)  NULL DEFAULT NULL COMMENT '执行器任务handler',
+                                 `executor_param` varchar(512)  NULL DEFAULT NULL COMMENT '执行器任务参数',
+                                 `executor_block_strategy` varchar(50)  NULL DEFAULT NULL COMMENT '阻塞处理策略',
+                                 `executor_timeout` int NOT NULL DEFAULT 0 COMMENT '任务执行超时时间，单位秒',
+                                 `executor_fail_retry_count` int NOT NULL DEFAULT 0 COMMENT '失败重试次数',
+                                 `glue_type` varchar(50)  NOT NULL COMMENT 'GLUE类型',
+                                 `glue_source` mediumtext  NULL COMMENT 'GLUE源代码',
+                                 `glue_remark` varchar(128)  NULL DEFAULT NULL COMMENT 'GLUE备注',
+                                 `glue_updatetime` datetime NULL DEFAULT NULL COMMENT 'GLUE更新时间',
+                                 `child_jobid` varchar(255)  NULL DEFAULT NULL COMMENT '子任务ID，多个逗号分隔',
+                                 `trigger_status` tinyint NOT NULL DEFAULT 0 COMMENT '调度状态：0-停止，1-运行',
+                                 `trigger_last_time` bigint NOT NULL DEFAULT 0 COMMENT '上次调度时间',
+                                 `trigger_next_time` bigint NOT NULL DEFAULT 0 COMMENT '下次调度时间',
+                                 `tenant_id` bigint NULL DEFAULT NULL,
+                                 PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_info
+-- ----------------------------
+INSERT INTO `xxl_job_info` VALUES (1, 1, '测试任务1', '2018-11-03 22:21:31', '2023-03-14 08:54:15', '迪卢克', 'weicj0426@163.com', 'CRON', '0/10 * * * * ? *', 'DO_NOTHING', 'FIRST', 'demoJobHandler', '222', 'COVER_EARLY', 60, 0, 'BEAN', '', 'GLUE代码初始化', '2018-11-03 00:00:00', NULL, 0, 0, 0, 9999);
+INSERT INTO `xxl_job_info` VALUES (4, 1, '测试任务2', '2023-03-16 11:36:47', '2023-03-16 11:36:47', '可莉', 'weicj0426@163.com', 'CRON', '0/10 * * * * ? *', 'DO_NOTHING', 'FIRST', 'demoJobHandler', '222', 'COVER_EARLY', 60, 1, 'BEAN', '', 'GLUE代码初始化', '2023-03-16 11:36:47', NULL, 0, 0, 0, 9999);
+
+-- ----------------------------
+-- Table structure for xxl_job_lock
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_lock`;
+CREATE TABLE `xxl_job_lock`  (
+                                 `lock_name` varchar(50)  NOT NULL COMMENT '锁名称',
+                                 `tenant_id` bigint NULL DEFAULT NULL,
+                                 PRIMARY KEY (`lock_name`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_lock
+-- ----------------------------
+INSERT INTO `xxl_job_lock` VALUES ('schedule_lock', 9999);
+
+-- ----------------------------
+-- Table structure for xxl_job_log
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_log`;
+CREATE TABLE `xxl_job_log`  (
+                                `id` bigint NOT NULL AUTO_INCREMENT,
+                                `job_group` int NOT NULL COMMENT '执行器主键ID',
+                                `job_id` int NOT NULL COMMENT '任务，主键ID',
+                                `executor_address` varchar(255)  NULL DEFAULT NULL COMMENT '执行器地址，本次执行的地址',
+                                `executor_handler` varchar(255)  NULL DEFAULT NULL COMMENT '执行器任务handler',
+                                `executor_param` varchar(512)  NULL DEFAULT NULL COMMENT '执行器任务参数',
+                                `executor_sharding_param` varchar(20)  NULL DEFAULT NULL COMMENT '执行器任务分片参数，格式如 1/2',
+                                `executor_fail_retry_count` int NOT NULL DEFAULT 0 COMMENT '失败重试次数',
+                                `trigger_time` datetime NULL DEFAULT NULL COMMENT '调度-时间',
+                                `trigger_code` int NOT NULL COMMENT '调度-结果',
+                                `trigger_msg` text  NULL COMMENT '调度-日志',
+                                `handle_time` datetime NULL DEFAULT NULL COMMENT '执行-时间',
+                                `handle_code` int NOT NULL COMMENT '执行-状态',
+                                `handle_msg` text  NULL COMMENT '执行-日志',
+                                `alarm_status` tinyint NOT NULL DEFAULT 0 COMMENT '告警状态：0-默认、1-无需告警、2-告警成功、3-告警失败',
+                                `tenant_id` bigint NULL DEFAULT NULL,
+                                PRIMARY KEY (`id`) USING BTREE,
+                                INDEX `I_trigger_time`(`trigger_time` ASC) USING BTREE,
+                                INDEX `I_handle_code`(`handle_code` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 433 CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_log
+-- ----------------------------
+INSERT INTO `xxl_job_log` VALUES (428, 1, 1, 'http://100.100.251.188:9999/', 'demoJobHandler', '', NULL, 0, '2023-03-16 11:35:24', 200, '任务触发类型：手动触发<br>调度机器：100.100.251.188<br>执行器-注册方式：自动注册<br>执行器-地址列表：[http://100.100.251.188:9999/]<br>路由策略：第一个<br>阻塞处理策略：覆盖之前调度<br>任务超时时间：60<br>失败重试次数：0<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>触发调度<<<<<<<<<<< </span><br>触发调度：<br>address：http://100.100.251.188:9999/<br>code：200<br>msg：null', '2023-03-16 11:35:34', 200, '', 0, 9999);
+INSERT INTO `xxl_job_log` VALUES (429, 1, 1, 'http://192.168.71.1:9999/', 'demoJobHandler', '', NULL, 0, '2023-03-16 21:08:23', 200, '任务触发类型：手动触发<br>调度机器：192.168.71.1<br>执行器-注册方式：自动注册<br>执行器-地址列表：[http://192.168.71.1:9999/]<br>路由策略：第一个<br>阻塞处理策略：覆盖之前调度<br>任务超时时间：60<br>失败重试次数：0<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>触发调度<<<<<<<<<<< </span><br>触发调度：<br>address：http://192.168.71.1:9999/<br>code：200<br>msg：null', '2023-03-16 21:08:33', 200, '', 0, 9999);
+INSERT INTO `xxl_job_log` VALUES (430, 1, 1, 'http://192.168.2.12:9999/', 'demoJobHandler', '', NULL, 0, '2023-03-18 13:31:54', 200, '任务触发类型：手动触发<br>调度机器：192.168.2.12<br>执行器-注册方式：自动注册<br>执行器-地址列表：[http://192.168.2.12:9999/]<br>路由策略：第一个<br>阻塞处理策略：覆盖之前调度<br>任务超时时间：60<br>失败重试次数：0<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>触发调度<<<<<<<<<<< </span><br>触发调度：<br>address：http://192.168.2.12:9999/<br>code：200<br>msg：null', '2023-03-18 13:32:04', 200, '', 0, 9999);
+INSERT INTO `xxl_job_log` VALUES (431, 1, 1, 'http://192.168.2.12:9999/', 'demoJobHandler', '', NULL, 0, '2023-03-18 13:32:29', 200, '任务触发类型：手动触发<br>调度机器：192.168.2.12<br>执行器-注册方式：自动注册<br>执行器-地址列表：[http://192.168.2.12:9999/]<br>路由策略：第一个<br>阻塞处理策略：覆盖之前调度<br>任务超时时间：60<br>失败重试次数：0<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>触发调度<<<<<<<<<<< </span><br>触发调度：<br>address：http://192.168.2.12:9999/<br>code：200<br>msg：null', '2023-03-18 13:32:39', 200, '', 0, 9999);
+INSERT INTO `xxl_job_log` VALUES (432, 1, 1, 'http://192.168.2.12:9999/', 'demoJobHandler', '', NULL, 0, '2023-03-18 13:35:01', 200, '任务触发类型：手动触发<br>调度机器：192.168.2.12<br>执行器-注册方式：自动注册<br>执行器-地址列表：[http://192.168.2.12:9999/]<br>路由策略：第一个<br>阻塞处理策略：覆盖之前调度<br>任务超时时间：60<br>失败重试次数：0<br><br><span style=\"color:#00c0ef;\" > >>>>>>>>>>>触发调度<<<<<<<<<<< </span><br>触发调度：<br>address：http://192.168.2.12:9999/<br>code：200<br>msg：null', '2023-03-18 13:35:11', 200, '', 0, 9999);
+
+-- ----------------------------
+-- Table structure for xxl_job_log_report
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_log_report`;
+CREATE TABLE `xxl_job_log_report`  (
+                                       `id` int NOT NULL AUTO_INCREMENT,
+                                       `trigger_day` datetime NULL DEFAULT NULL COMMENT '调度-时间',
+                                       `running_count` int NOT NULL DEFAULT 0 COMMENT '运行中-日志数量',
+                                       `suc_count` int NOT NULL DEFAULT 0 COMMENT '执行成功-日志数量',
+                                       `fail_count` int NOT NULL DEFAULT 0 COMMENT '执行失败-日志数量',
+                                       `update_time` datetime NULL DEFAULT NULL,
+                                       `tenant_id` bigint NULL DEFAULT NULL,
+                                       PRIMARY KEY (`id`) USING BTREE,
+                                       UNIQUE INDEX `i_trigger_day`(`trigger_day` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 816 CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_log_report
+-- ----------------------------
+INSERT INTO `xxl_job_log_report` VALUES (1, '2022-10-27 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (2, '2022-10-26 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (3, '2022-10-25 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (4, '2022-10-31 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (5, '2022-10-30 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (6, '2022-10-29 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (7, '2022-11-01 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (8, '2022-11-02 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (9, '2022-11-03 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (10, '2022-11-09 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (11, '2022-11-08 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (12, '2022-11-07 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (13, '2022-11-10 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (14, '2022-11-12 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (15, '2022-11-11 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (16, '2022-11-13 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (17, '2022-12-31 00:00:00', 0, 0, 1, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (18, '2022-12-30 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (19, '2022-12-29 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (20, '2023-01-01 00:00:00', 0, 0, 1, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (21, '2023-01-03 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (22, '2023-01-02 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (23, '2023-01-04 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (24, '2023-01-11 00:00:00', 2, 0, 7, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (25, '2023-01-10 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (26, '2023-01-09 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (27, '2023-01-15 00:00:00', 0, 0, 1, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (28, '2023-01-14 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (29, '2023-01-13 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (30, '2023-01-18 00:00:00', 0, 0, 1, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (31, '2023-01-17 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (32, '2023-01-16 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (33, '2023-01-19 00:00:00', 0, 0, 1, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (34, '2023-01-20 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (35, '2023-01-23 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (36, '2023-01-22 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (37, '2023-01-21 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (38, '2023-01-28 00:00:00', 0, 0, 3, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (39, '2023-01-27 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (40, '2023-01-26 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (407, '2023-01-29 00:00:00', 0, 0, 0, NULL, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (800, '2023-03-02 00:00:00', 0, 2, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (801, '2023-03-01 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (802, '2023-02-28 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (803, '2023-03-08 00:00:00', 0, 1, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (804, '2023-03-07 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (805, '2023-03-06 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (806, '2023-03-10 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (807, '2023-03-09 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (808, '2023-03-11 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (809, '2023-03-13 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (810, '2023-03-12 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (811, '2023-03-14 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (812, '2023-03-15 00:00:00', 0, 0, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (813, '2023-03-16 00:00:00', 0, 2, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (814, '2023-03-18 00:00:00', 0, 3, 0, NULL, 9999);
+INSERT INTO `xxl_job_log_report` VALUES (815, '2023-03-17 00:00:00', 0, 0, 0, NULL, 9999);
+
+-- ----------------------------
+-- Table structure for xxl_job_logglue
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_logglue`;
+CREATE TABLE `xxl_job_logglue`  (
+                                    `id` int NOT NULL AUTO_INCREMENT,
+                                    `job_id` int NOT NULL COMMENT '任务，主键ID',
+                                    `glue_type` varchar(50)  NULL DEFAULT NULL COMMENT 'GLUE类型',
+                                    `glue_source` mediumtext  NULL COMMENT 'GLUE源代码',
+                                    `glue_remark` varchar(128)  NOT NULL COMMENT 'GLUE备注',
+                                    `add_time` datetime NULL DEFAULT NULL,
+                                    `update_time` datetime NULL DEFAULT NULL,
+                                    `tenant_id` bigint NULL DEFAULT NULL,
+                                    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_logglue
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for xxl_job_registry
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_registry`;
+CREATE TABLE `xxl_job_registry`  (
+                                     `id` int NOT NULL AUTO_INCREMENT,
+                                     `registry_group` varchar(50)  NOT NULL,
+                                     `registry_key` varchar(255)  NOT NULL,
+                                     `registry_value` varchar(255)  NOT NULL,
+                                     `update_time` datetime NULL DEFAULT NULL,
+                                     `tenant_id` bigint NULL DEFAULT NULL,
+                                     PRIMARY KEY (`id`) USING BTREE,
+                                     INDEX `i_g_k_v`(`registry_group` ASC, `registry_key` ASC, `registry_value` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_registry
+-- ----------------------------
+INSERT INTO `xxl_job_registry` VALUES (12, 'EXECUTOR', 'xxl-job-executor-sample', 'http://192.168.2.12:9999/', '2023-03-18 20:20:57', 9999);
+
+-- ----------------------------
+-- Table structure for xxl_job_user
+-- ----------------------------
+DROP TABLE IF EXISTS `xxl_job_user`;
+CREATE TABLE `xxl_job_user`  (
+                                 `id` int NOT NULL AUTO_INCREMENT,
+                                 `username` varchar(50)  NOT NULL COMMENT '账号',
+                                 `password` varchar(50)  NOT NULL COMMENT '密码',
+                                 `role` tinyint NOT NULL COMMENT '角色：0-普通用户、1-管理员',
+                                 `permission` varchar(255)  NULL DEFAULT NULL COMMENT '权限：执行器ID列表，多个逗号分割',
+                                 `tenant_id` bigint NULL DEFAULT NULL,
+                                 PRIMARY KEY (`id`) USING BTREE,
+                                 UNIQUE INDEX `i_username`(`username` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 ;
+
+-- ----------------------------
+-- Records of xxl_job_user
+-- ----------------------------
 BEGIN;
 INSERT INTO `sys_user_role` VALUES (1, 1, 9999);
 COMMIT;
