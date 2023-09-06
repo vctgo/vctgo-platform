869f6831f7bf09e438192c58531636ca43c4dff9
donghaoran <dhr92@163.com>
2023-09-06 22:23:52 +0800

    [FEATURE]1.调整图标展示,优化部分代码

--- vctgo-ui/src/components/TopNav/index.vue
+++ vctgo-ui/src/components/TopNav/index.vue
@@ -6,9 +6,11 @@
   >
     <template v-for="(item, index) in topMenus">
       <el-menu-item :style="{'--theme': theme}" :index="item.path" :key="index" v-if="index < visibleNumber"
-      ><svg-icon :icon-class="item.meta.icon" />
-        {{ item.meta.title }}</el-menu-item
-      >
+      ><svg-icon
+        v-if="item.meta && item.meta.icon && item.meta.icon !== '#'"
+        :icon-class="item.meta.icon"
+      />
+        {{ item.meta.title }}</el-menu-item>
     </template>
 
     <!-- 顶部菜单超出数量折叠 -->
